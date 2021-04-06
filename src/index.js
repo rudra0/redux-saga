f7fe752bf8c7693dacb6cf1323af407248d55354
rpsingh <rpsingh@craterzone.com>
2021-04-06 13:34:29 +0530

    new file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { Counter } from "./Counter";
+import { Counter } from "./Login";
 import { store } from "./init-store.js";
 import { Provider } from "react-redux";
 
